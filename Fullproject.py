9dcefdbfccc60700994a75bdf22cb7d8c9f93d75
shipko-stepan <sshypko@kse.org.ua>
2022-12-22 04:12:44 +0200

    task1

--- Fullproject.py
+++ Fullproject.py
@@ -54,3 +54,67 @@ def print_medals(result: list):
 def print_totals(result: dict):
     for k, v in result.items():
         print(f'{k} - Gold: {v["Gold"]} - Silver: {v["Silver"]} - Bronze: {v["Bronze"]}')
+
+
+def task1(filepath, medals, output=None):
+    requested_country, requested_year = medals
+
+    result = []
+
+    bronze = 0
+    silver = 0
+    gold = 0
+
+    all_years = set()
+    all_countries = set()
+
+    with open(filepath) as f:
+        f.readline()  # skip header
+        next_line = f.readline()
+        count = 0
+        while next_line:
+            record = next_line.split('\t')
+            name = record[1].strip()
+            country = record[6].strip()
+            year = int(record[9].strip())
+            sport = record[12].strip()
+            noc = record[7].strip()
+            medal = record[14].strip()
+
+            countries = {country, noc}
+
+            all_years.add(year)
+            all_countries.add(country)
+            all_countries.add(noc)
+
+            if not all([int(requested_year) == year, requested_country in countries]):
+                next_line = f.readline()
+                continue
+
+            if requested_country in (country, noc):
+                if medal != 'NA':
+                    count += 1
+                    result.append({"Record": f'{count} - {name} - {sport} - {medal}'})
+
+                    if medal == 'Bronze':
+                        bronze += 1
+                    elif medal == 'Silver':
+                        silver += 1
+                    else:
+                        gold += 1
+
+            if count == 10:
+                result.append({'Medals': f'Gold: {gold}, Silver: {silver}, Bronze: {bronze}'})
+                print_medals(result)
+                if output:
+                    write_output_medals_to_file(output, result)
+                break
+
+            next_line = f.readline()
+
+        if int(requested_year) not in all_years:
+            print(f'In {requested_year} Olympics were not conducted')
+        elif requested_country not in all_countries:
+            print(f'{requested_country} didn\'t participate in Olympics')
+        elif len(result) < 10:
+            print('There are less than 10 medals')
