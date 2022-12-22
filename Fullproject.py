2dc67ec5ad0517340f1c5146e583a85d56c12a73
shipko-stepan <sshypko@kse.org.ua>
2022-12-22 04:07:28 +0200

    start

--- Fullproject.py
+++ Fullproject.py
@@ -1,31 +1,28 @@
 import argparse
-parser = argparse.ArgumentParser(description="our example parser.")
-parser.add_argument("--filename","-f",required=True)
-parser.add_argument("--medals",action="store_true",required=False)
-parser.add_argument("--output","-o",required=False)
-parser.add_argument("--total","-t",required=False)
-parser.add_argument("--year","-y",required=False)
-parser.add_argument("--team",required=False)
+import sys
 
-args=parser.parse_args()
 
-filename = args['filename']
-medals = args['medals']
-output = args['output']
-total = args['total']
-overall = args['overall']
-interactive = args['interactive']
-file= open("filename","r")
+def main():
+    parser = argparse.ArgumentParser(description='File processor CLI')
+    parser.add_argument('--filepath', '-f', help='Path to the file to be processed', required=True)
+    parser.add_argument('--output', '-out', help='Filename for output results', required=False)
 
-def part1(filename,country,year,output):
-    zoloto=0
-    serebro=0
-    bronz=0
-    i=0
-    x=''
-    while True:
-        line=file.readline()
-        if not line:
-            break
-        id=line.split("\t")
-        year1=id[]
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--medals', '-m', nargs=2, help='Get data on the 10 medalists for a specific country in a' ' specific year')
+    group.add_argument('--total', '-t', help='Get data about countries which got at least one medal in specific year')
+    group.add_argument('--overall', '-o', nargs='+', help='Get the year for specific countries when they won the'' most medals')
+    group.add_argument('--interactive', '-i', help='Interactive mode', action='store_true')
+
+    args = parser.parse_args()
+
+    if not args.filepath:
+        parser.print_help()
+        sys.exit(1)
+    if args.medals:
+        task1(args.filepath, args.medals, args.output)
+    elif args.total:
+        task2(args.filepath, args.total, args.output)
+    elif args.overall:
+        task3(args.filepath, args.overall, args.output)
+    elif args.interactive:
+        task4(args.filepath)
